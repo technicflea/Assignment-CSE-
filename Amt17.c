61e44302496cfeaca3accc7ab202014ff0679004
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-07 18:20:18 +0530

    Update Amt17.c

--- Amt17.c
+++ Amt17.c
@@ -1,1 +1,14 @@
-
+/*program to print the sum of last two digits*/
+#include<stdio.h>
+void main()
+  {
+  int n;
+  printf("please enter the no. of which you want to add last  two digits");
+  if(n/10==0)
+    {printf("no. has less than two digits");
+    }
+  else
+    n=n%100;
+    n=(n%10)+(n/10);
+  printf("the sum of last two digits is %d",n);
+  }
