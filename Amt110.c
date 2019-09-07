029e9a5e03d28e25b2decfeb1d59f1a285e0621c
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-08 00:45:30 +0530

    Update Amt110.c

--- Amt110.c
+++ Amt110.c
@@ -1,1 +1,19 @@
+/* program to exchange the last 2 digits of a no.*/
+#include<stdio.h>
 
+int main() 
+{
+    int n; 
+    printf("please enter the no. of which you want to exchange last two digits");
+    scanf("%d",&n);
+    if(n/10==0)
+    { printf("the no. is single digit");
+    }
+    else
+    n=(n-(n%100))+((n%100)/10)+ ((n%10)*10);
+    printf("modified no. =%d",n);
+    
+    
+    
+    
+}
