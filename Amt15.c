331404f7948d0492c28960069659cea1524c81a4
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-07 16:19:11 +0530

    Update Amt15.c

--- Amt15.c
+++ Amt15.c
@@ -5,11 +5,11 @@
 void main()
   {
   int n;
-  printf("please enter the no. of which you want to delete last digit");
+  printf("please enter the no. of which you want to delete last 2 digits");
   if(n/100==0)
     {printf("no. has less than 3 digits, no digit left after deletion");
     }
   else
     n=n/100;
-  printf("the no. after deletion of last digit is %d",n);
+  printf("the no. after deletion of last 2 digits is %d",n);
   }
