7299bbd577321f3556f8f10681d068feddc1725f
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-07 16:17:52 +0530

    Create Amt16.c

---
+++ Amt16.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+void main()
+  {
+  int n;
+  printf("please enter the no. of which you want to print second last digit");
+  if(n/10==0)
+    {printf("no. has less than 2 digit");
+    }
+  else
+    n=n%100;
+    n=n/10;
+  printf("the second last digit  is %d",n);
+  }
