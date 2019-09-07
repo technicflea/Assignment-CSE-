f6f068fb7cfa3020471e7c92cfa774646892ba13
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-08 01:11:27 +0530

    Update Amt22.c

--- Amt22.c
+++ Amt22.c
@@ -1,1 +1,13 @@
-
+/" program to find the distance between two input points*/
+ #include<stdio.h>
+  #include<math.h>
+  int main()
+  {
+  float a,b,c,d, dist;
+  printf("please enter the coordinates of points between which u want to find distance\n point1(a,b)=");
+  scanf("%f%f",&a,&b);
+  printf(" point2(c,d)=");
+  scanf("%f%f",&c,&d);
+  dist=sqrt(pow(a-c,2)-pow(b-d,2));
+  printf("the distance=%f", dist);
+  }
