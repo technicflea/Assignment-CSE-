ec357e37d7a55a8033aad7b432552a9e1b9a804e
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-08 00:01:34 +0530

    Update Amt19.c

--- Amt19.c
+++ Amt19.c
@@ -1,1 +1,16 @@
 
+/*program to delete 2nd last digit*/
+#include<stdio.h>
+void main()
+  {
+  int n;
+  printf("please enter the no. of which you want to delete 2nd last digit");
+  scanf("%d",n);
+  if(n/10==0)
+    {printf("no. has only one digit")
+     }
+  else
+    {n=(n/100)*10+(n%10);
+     }
+  printf("the no. after deleting 2nd last digit=%d",n)
+  }
