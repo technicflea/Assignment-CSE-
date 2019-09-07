306620ee3651b6f1a6e0cecad3aba1309fb63181
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-08 01:01:23 +0530

    Update Amt21.c

--- Amt21.c
+++ Amt21.c
@@ -1,1 +1,14 @@
+/* this program calculated the area of triangle inputs are sides*/
+#include<stdio.h>
+#include<math.h>
+int main()
+  {
+  int a,b,c;
+  float s, area=0;
+  printf(" please enter the sides a,b,c of triangle");
+  scanf("%d%d%d", &a,&b,&c);
+  s=(a+b+c)/2;
+  area=sqrt(s*(s-a)*(s-b)*(s-c));
+  printf("area of triangle=%f",area);
+  }
 
