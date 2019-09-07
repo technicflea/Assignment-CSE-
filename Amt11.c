26dce4caf11b6f7840e4113b3f58edc869f68998
chhaviyush <54477505+chhaviyush@users.noreply.github.com>
2019-09-07 13:32:36 +0530

    Update Amt11.c

--- Amt11.c
+++ Amt11.c
@@ -1,1 +1,12 @@
+/* this program is to print area of rectangle*/
+//author:chhaviyush kusumakar
+#include<stdio.h>
+void main()
+  {
+  int l,b,area=0;
+  printf("please enter the length and breadth of rectangle");
+  scanf("%d%d",&l,&b);
+  area=l*b;
+  printf("%d",area);
+  }
 
