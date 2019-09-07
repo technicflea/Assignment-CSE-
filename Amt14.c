d362815fcd1383184840b0809e76bb5184bd199f
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-07 14:32:19 +0530

    Update Amt14.c

--- Amt14.c
+++ Amt14.c
@@ -1,1 +1,13 @@
-
+/*program to  delete last digit of a no.*/
+#include<stdio.h>
+void main()
+  {
+  int n;
+  printf("please enter the no. of which you want to delete last digit");
+  if(n/10==0)
+    {printf("no. is single digit, no digit left after deletion");
+    }
+  else
+    n=n/10;
+  printf("the no. after deletion of last digit is %d",n);
+  }
