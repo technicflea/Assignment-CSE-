53b135fb6cb1c774c30ed9f7e2e33fc322ccc817
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-07 18:48:37 +0530

    Update Amt18.c

--- Amt18.c
+++ Amt18.c
@@ -1,1 +1,16 @@
-
+/* program to double the last digit of no.*/
+#include<stdio.h>
+void main()
+  {
+  int n;
+  printf("please enter the no. of which you want to double the last digit");
+  starts:
+  printf("please keep the last digit less than 5");
+  scanf("%d",&n);
+    if(n%10<=5)
+      { goto starts;
+      }
+    else
+      n=n+(n%10);
+  printf("the modified no. is %d",n);
+  }
