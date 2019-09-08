b705fb1bb63d482e87a97768246fb11c8aadf76b
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-08 09:06:07 +0530

    Update Amy23.c

--- Amy23.c
+++ Amy23.c
@@ -2,7 +2,7 @@
  #include<math.h>
   int main()
   {
-  float a,b,c,d, l,m,n;
+  float a,b,c,d,e,f, l,m,n;
   printf("please enter the coordinates of vertices of triangle \n point1(a,b)=");
   scanf("%f%f",&a,&b);
   printf(" point2(c,d)=");
@@ -12,5 +12,10 @@
   l=sqrt(pow(a-c,2)-pow(b-d,2));
     m=sqrt(pow(a-e,2)-pow(b-f,2));
     n=sqrt(pow(e-c,2)-pow(f-d,2))
-  printf("the distance=%f", dist);
+    float s, area=0;
+  
+
+  s=(l+m+n)/2;
+  area=sqrt(s*(s-l)*(s-m)*(s-n));
+  printf("area of triangle=%f",area);
   }
