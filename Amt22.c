c068ddc39780e954288812ca5f661223ac27171d
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-10 10:58:39 +0530

    Update Amt22.c

--- Amt22.c
+++ Amt22.c
@@ -8,6 +8,6 @@
   scanf("%f%f",&a,&b);
   printf(" point2(c,d)=");
   scanf("%f%f",&c,&d);
-  dist=sqrt(pow(a-c,2)-pow(b-d,2));
+  dist=sqrt(pow(a-c,2)+pow(b-d,2));
   printf("the distance=%f", dist);
   }
