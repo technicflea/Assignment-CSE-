1eca5733ec644cea929f44f7a39e31e4a0f41a06
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-10 10:59:38 +0530

    Update Amt23.c

--- Amt23.c
+++ Amt23.c
@@ -9,9 +9,9 @@
   scanf("%f%f",&c,&d);
   printf("point3(e,f)=");
   scanf("%f%f",&e,&f);
-  l=sqrt(pow(a-c,2)-pow(b-d,2));
-    m=sqrt(pow(a-e,2)-pow(b-f,2));
-    n=sqrt(pow(e-c,2)-pow(f-d,2));
+  l=sqrt(pow(a-c,2)+pow(b-d,2));
+    m=sqrt(pow(a-e,2)+pow(b-f,2));
+    n=sqrt(pow(e-c,2)+pow(f-d,2));
     float s, area=0;
   
 
