f59abb57b7fddb95e78101d3c75a0de5e7a85ba9
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-13 00:25:50 +0530

    Create Amt39.c

---
+++ Amt39.c
@@ -0,0 +1,18 @@
+/*equality of x with four other nos*/
+#include<stdio.h>
+void main()
+{
+    int a,b,c,d,x;
+    printf("enter five nos. where last no. is equal to one of the four");
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&x);
+    if (x==a)
+    printf("x=a");
+    if(x==b)
+    printf("x=b");
+    if(x==c)
+    printf("x=c");
+    if (x==d)
+    printf("x=d");
+    
+    
+}
