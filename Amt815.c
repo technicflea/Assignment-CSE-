cf7b4e4b1ef5518d6ea682408838f009634f3e6e
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-17 22:55:36 +0530

    Create Amt815.c

---
+++ Amt815.c
@@ -0,0 +1,21 @@
+ #include<stdio.h>
+
+int main() 
+{
+    int i,n,x,sum=0,count=0;
+    printf("give n ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&x);
+        if(x%2==0)
+        {
+          sum=sum=(i*x);
+          count++;
+        }
+        
+    }
+    int avg;
+    avg=sum/count;
+    printf("\n%d",avg);
+} 
