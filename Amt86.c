856686a0df57d3b84dc99a06cc765fdc851a4214
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-17 22:33:40 +0530

    Create Amt86.c

---
+++ Amt86.c
@@ -0,0 +1,15 @@
+ #include<stdio.h>
+
+int main() 
+{
+    int i,n,x,sum=0;
+    printf("give n ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&x);
+        sum=sum+((x%100)/10)+((x%10)*10);
+        
+    }
+    printf("\n%d",sum);
+} 
