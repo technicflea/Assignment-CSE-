b499371cd944e7a386ee6b754e3715f4a56228aa
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-22 17:28:07 +0530

    Create Amt46.c

---
+++ Amt46.c
@@ -0,0 +1,19 @@
+  /* program to print numbers less than 20, bw 50 to 70 and more than 90 in interval 0 to 100 */
+#include<stdio.h>
+int main()
+{
+	int i;
+printf("Numbers bw 50 to 70, less than 20 and more than 90 are");
+for(i=0; i<=100; i++)
+{
+	if(i>=20 && i<=40 || i<=90 && i>=70)
+	{
+	}
+	else
+	{
+  printf("%d\n", i);
+	}
+}
+ 
+ 
+ }
