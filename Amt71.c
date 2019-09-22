d63162d34c1c3c6f33f2b3c200517e6bee0ce276
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-22 13:03:11 +0530

    Create Amt71.c

---
+++ Amt71.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() 
+{
+    unsigned int a,b,c,d;
+    printf("please enter the values of a, b, c,d\n");
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    printf("%d\t%d\n%d\t%d",a,b,c,d);
+    return 0;
+}
