cd360d7e186989579158e25d6d79dbccb5088b5a
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-22 17:30:03 +0530

    Create Amt49.c

---
+++ Amt49.c
@@ -0,0 +1,19 @@
+/*To print a number whose last digit is multiple of 3 in 0-100*/
+#include<stdio.h>
+int main()
+{
+	int j,i;
+for(i=0; i<=100; i++)
+{ 
+j= i%10;
+	{
+		if ( j%3==0)
+{
+	printf("%d\n", i);
+	}	
+}
+ 
+}
+
+}
+ 
