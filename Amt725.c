f934dee6068b6496bc1f5184b43eeabdf60561ab
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-23 00:17:39 +0530

    Create Amt725.c

---
+++ Amt725.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+
+int main() 
+{
+    int i,j;
+    printf("enter 2 no.");
+    scanf("%1d%1d",&i,&j);
+    printf("the product of first digits is %d",i*j);
+    return 0;
+    
+}
