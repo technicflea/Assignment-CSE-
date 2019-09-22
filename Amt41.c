f214e85a794efe8a529aac5ccac38b9f489b5aa1
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-22 17:15:57 +0530

    Create Amt41.c

---
+++ Amt41.c
@@ -0,0 +1,13 @@
+/*program to print even no. bw 20 to 70*/
+#include<stdio.h>
+int main()
+{
+	int i;
+	printf("Even number bw 20 and 70 ");
+for(i=20; i<=70; i++)
+{	if ( i%2==0)
+		{
+			printf("%d\n", i) ;
+		}
+	}
+}
