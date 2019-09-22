47f05b6dca1280dc54cefbd09eedeb5047494c83
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-22 23:55:42 +0530

    Create Amt722.c

---
+++ Amt722.c
@@ -0,0 +1,13 @@
+//proper vector printing
+#include<stdio.h>
+#include<math.h>
+
+int main() 
+{
+    int i;
+    printf("enter a no.");
+    scanf("%1d",&i);
+    printf("the first digit is %1d",i);
+    return 0;
+    
+}
