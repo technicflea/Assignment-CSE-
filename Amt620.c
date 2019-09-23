78b801f33426236d6a66f68a18d64e00471337a0
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-23 16:52:07 +0530

    Create Amt620.c

---
+++ Amt620.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int i,j,l=65;
+for(i=1;i<=6;i++)
+   {
+   for(j=1;j<=i;j++)
+       {
+       printf("%c",l);
+       l++;
+       }   
+   printf("\n");
+   }
+}
