f573f4d5290b4f8174d9c2c1648a24b5237496ce
Technic flea <54477505+technicflea@users.noreply.github.com>
2019-09-23 17:21:33 +0530

    Update Amt511.c

--- Amt511.c
+++ Amt511.c
@@ -1,38 +1,10 @@
 #include<stdio.h>
 void main()
 {
-    int x,y,i;
-    for(i=0;i<10;i++)
+    int i,j;
+    printf("0");
+    for(i=0;i<7;i++)
     {
-        if(i%2==0)
-        {   for(x=0;x<20;x++)
-            {
-                printf("*");
-            }
-        printf("\n");
-        }
-        if(i==1 || i==3)
-        {
-            for(x=0;x<20;x++)
-            {
-                printf("0");
-            }
-        printf("\n");
-        }
-        if(i==5 || i==7 || i==9)
-        {
-            for(x=0;x<20;x++)
-            {
-                if(x<4 || x>13)
-                {
-                    printf("0");
-                }
-                else
-                {
-                    printf("*");
-                }
-            }
-        printf("\n");
-        }
+        printf("0*");
     }
 }
